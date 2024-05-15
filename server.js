de732e2f73a302af0105a65a6bd16051b7111d50
MUKHAMMADYUSUF <mfdev98@gmail.com>
2024-05-15 20:41:19 +0900

    fix: 18 to 19

--- server.js
+++ server.js
@@ -18,12 +18,28 @@ app.set("view engine", "ejs");
 //     res.end(`<h1 style="background:red">HELLO WORLD by Muhammadyusuf</h1>`);
 // });
 
-app.get("/hello", function(req, res) {
-    res.end("<h1>HELLO WORLD</h1>");
+// app.get("/hello", function(req, res) {
+//     res.end("<h1>HELLO WORLD</h1>");
+// });
+
+// app.get("/gift", function(req, res) {
+//     res.end("<h1>Siz sovgalar bo'limidasiz</h1>");
+// });
+
+// app.get("/", function(req, res) {
+//     res.render('project');
+// });
+
+app.post("/create-item", (req, res) => {
+    // console.log(req);
+    console.log(req.body);
+    res.json({test: "success" });
+})
+
+app.get("/", function(req, res) {
+    res.render("harid");
 });
-app.get("/gift", function(req, res) {
-    res.end("<h1>Siz sovgalar bo'limidasiz</h1>");
-});
+
 
 
 const server = http.createServer(app);
