b970b61d4e291c10e4408bd74b379db7b8b5b6c5
MUKHAMMADYUSUF <mfdev98@gmail.com>
2024-05-16 21:29:53 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,39 +2,34 @@ console.log("Web serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err){
+        console.log("ERROR", err);
+    } else{
+        user = JSON.parse(data); 
+    }
+});
 
 // 1: Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));  
+app.use(express.json());    
+app.use(express.urlencoded({extended: true})); 
 
 // 2: Session
-// 3: Views code
+// 3: Views code            
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-// 4 Routing code
-// app.get("/", function(req, res) {
-//     res.end(`<h1 style="background:red">HELLO WORLD by Muhammadyusuf</h1>`);
-// });
+// 4 Routing code           
+app.post("/create-item", (req, res) => {    
 
-// app.get("/hello", function(req, res) {
-//     res.end("<h1>HELLO WORLD</h1>");
-// });
-
-// app.get("/gift", function(req, res) {
-//     res.end("<h1>Siz sovgalar bo'limidasiz</h1>");
-// });
-
-// app.get("/", function(req, res) {
-//     res.render('project');
-// });
-
-app.post("/create-item", (req, res) => {
-    // console.log(req);
-    console.log(req.body);
-    res.json({test: "success" });
-})
+});
+app.get("/author", function(req, res) {
+    res.render("author", {user: user});
+});
 
 app.get("/", function(req, res) {
     res.render("harid");
