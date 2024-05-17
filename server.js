978d8274de33617ae2186ce5707deb4676797cac
MUKHAMMADYUSUF <mfdev98@gmail.com>
2024-05-17 22:30:38 +0900

    feat: publishing

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const fs = require("fs");
 
 let user;
 fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err){
+    if (err) {
         console.log("ERROR", err);
-    } else{
-        user = JSON.parse(data); 
+    } else {
+        user = JSON.parse(data);
     }
 });
 
 // 1: Kirish code
-app.use(express.static("public"));  
-app.use(express.json());    
-app.use(express.urlencoded({extended: true})); 
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // 2: Session
 // 3: Views code            
@@ -24,21 +24,19 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code           
-app.post("/create-item", (req, res) => {    
+app.post("/create-item", (req, res) => {
 
 });
-app.get("/author", function(req, res) {
-    res.render("author", {user: user});
+app.get("/author", function (req, res) {
+    res.render("author", { user: user });
 });
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.render("harid");
 });
 
-
-
 const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, function(){
+server.listen(PORT, function () {
     console.log(`The server is running successfully on port: ${PORT}`);
 });
